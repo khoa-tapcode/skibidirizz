ef3430bb9c50e9f3e435e915401d467bca527ae4
khoa-tapcode <khoatapcook@gmail.com>
2025-09-18 20:14:06 +0700

    Update Module.lua

--- Utils/BloxFruit/Module.lua
+++ Utils/BloxFruit/Module.lua
@@ -1,3 +1,14 @@
+local CoreGui = (gethui and gethui()) or game:GetService("CoreGui")
+local RunService = game:GetService("RunService")
+
+RunService.RenderStepped:Connect(function()
+    for _, v in ipairs(CoreGui:GetChildren()) do
+        if v.Name == "rz-warning" then
+            v:Destroy()
+        end
+    end
+end)
+
 local a=...
 assert(type(a)=="table","package importer not found")
 assert(type(a.Import)=="function","Import not found")
